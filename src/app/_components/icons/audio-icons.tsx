496788d8933c7698138d21e167e0086646a7bde9
251207 <ethan.jang@obigo.com>
2026-02-27 11:48:31 +0900

    chore: update src/app/_components/icons/audio-icons.tsx

---
+++ src/app/_components/icons/audio-icons.tsx
@@ -0,0 +1,45 @@
+export function PlayIcon() {
+  return (
+    <svg viewBox='0 0 24 24' className='h-4 w-4' aria-hidden='true'>
+      <path d='M8 5v14l11-7z' fill='currentColor' />
+    </svg>
+  );
+}
+
+export function PauseIcon() {
+  return (
+    <svg viewBox='0 0 24 24' className='h-4 w-4' aria-hidden='true'>
+      <path d='M7 5h4v14H7zM13 5h4v14h-4z' fill='currentColor' />
+    </svg>
+  );
+}
+
+export function ResetIcon() {
+  return (
+    <svg viewBox='0 0 24 24' className='h-4 w-4' aria-hidden='true'>
+      <path
+        d='M12 5a7 7 0 1 1-6.7 9h2.1A5 5 0 1 0 12 7a4.9 4.9 0 0 0-3.5 1.5L11 11H4V4l3.1 3.1A7 7 0 0 1 12 5z'
+        fill='currentColor'
+      />
+    </svg>
+  );
+}
+
+export function MinimizeIcon() {
+  return (
+    <svg viewBox='0 0 24 24' className='h-4 w-4' aria-hidden='true'>
+      <path d='M5 11h14v2H5z' fill='currentColor' />
+    </svg>
+  );
+}
+
+export function CloseIcon() {
+  return (
+    <svg viewBox='0 0 24 24' className='h-4 w-4' aria-hidden='true'>
+      <path
+        d='M18.3 5.7 12 12l6.3 6.3-1.4 1.4L10.6 13.4 4.3 19.7l-1.4-1.4L9.2 12 2.9 5.7 4.3 4.3l6.3 6.3 6.3-6.3z'
+        fill='currentColor'
+      />
+    </svg>
+  );
+}
