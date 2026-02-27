13c50cc696a5232425069f0ee080e54425d2aece
251207 <ethan.jang@obigo.com>
2026-02-27 11:01:20 +0900

    chore: update src/app/sync/contents/page.tsx

---
+++ src/app/sync/contents/page.tsx
@@ -0,0 +1,5 @@
+import { CountryContentsPageClient } from '../../_components/pages/country-contents-page-client';
+
+export default function CountryContentsPage() {
+  return <CountryContentsPageClient />;
+}
